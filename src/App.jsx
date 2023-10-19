3adca0ce03d81e48224e1650b21fbc62562d4788
Vipin-chauhan <vipin.chauhan@thewitslab.com>
2023-10-19 15:35:48 +0530

    redux toolkit project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+
+
+function App() {
+  return (
+    <>
+      <h1>Learn Redux</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
